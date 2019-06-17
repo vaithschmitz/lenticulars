8649edcffcbba3cc6e2491ce419f3a599661f7a5
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-06-17 14:20:08 +0100

    .

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ window.addEventListener('deviceorientation', function(event) {
 
     if (event.gamma >= 73 && event.gamma <= 89 ){
         $('images').textContent =  Math.floor(event.gamma)
-        $('images').style.backgroundImage =  "url('./careworker.png')"
+        $('images').style.backgroundImage =  "url('./careworker.png') background-size: contain;"
     }
     else if ( event.gamma >= 45 && event.gamma <= 72 ){
         $('images').textContent =  Math.floor(event.gamma)
-        $('images').style.backgroundImage =  "url('confidant.png')"
+        $('images').style.backgroundImage =  "url('confidant.png') background-size: contain;"
     }
     else{
         $('images').textContent =  Math.floor(event.gamma)
-        $('images').style.backgroundImage =  "url('teammate.png')"
+        $('images').style.backgroundImage =  "url('teammate.png') background-size: contain;"
     }
   });
 

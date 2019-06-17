c74e52fc5294506794b303c1b8ba98e7cb9c8560
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-06-17 15:20:33 +0100

    .

--- index.js
+++ index.js
@@ -8,23 +8,20 @@ window.addEventListener('deviceorientation', function(event) {
     x = Math.floor(event.gamma)
     y = Math.floor(event.beta)
 
-    if (x <= -76 && x >= -91 || x >= 75 && x <= 91) vertical{
+    if (x <= -76 && x >= -91 || x >= 75 && x <= 91){
         $('images').classList.remove('careworker')
         $('images').classList.remove('confidant')
         $('images').classList.add('teammate')
-        $('images').textContent = y
     }
-    else if ( x <= -1 && event.gamma >= -75  ){
+    else if ( x <= -1 && x >= -75  ){
         $('images').classList.remove('teammate')
         $('images').classList.remove('confidant')
         $('images').classList.add('careworker')
-        $('images').textContent = y
     }
     else{
         $('images').classList.remove('teammate')
         $('images').classList.remove('careworker')
         $('images').classList.add('confidant')
-        $('images').textContent = y
 
     }
   });

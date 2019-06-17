2097d4f0533cfd3e1d34cb6a0571b71757d0501d
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-06-17 13:28:10 +0100

    .

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ window.addEventListener('deviceorientation', function(event) {
     // $('images').textContent =  event.gamma
     if (event.gamma >= 0){
         $('images').textContent =  "HELLLO"
-        $('images').style.backgroundImage =  url('carers-2.jpg')
+        $('images').style.backgroundImage =  "url('./carers-2.jpg')"
     }
     else{
         $('images').textContent =  "Goodbye"
-        $('images').style.backgroundImage =  url('carers-1.jpg')
+        $('images').style.backgroundImage =  "url('carers-1.jpg')"
     }
   });
 

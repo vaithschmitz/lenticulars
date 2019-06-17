40955ddbfb8e084a69993cba362e8b3573b6ecea
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-06-17 14:09:42 +0100

    .

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ let x = ''
 window.addEventListener('deviceorientation', function(event) {
 
     if (event.gamma >= 0){
-        $('images').textContent =  event.gamma % 2
+        $('images').textContent =  Math.floor(event.gamma)
         $('images').style.backgroundImage =  "url('./careworker.png')"
     }
     else{
-        $('images').textContent =  event.gamma % 2
+        $('images').textContent =  Math.floor(event.gamma)
         $('images').style.backgroundImage =  "url('confidant.png')"
     }
   });

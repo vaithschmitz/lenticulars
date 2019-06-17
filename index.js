6fba0470f481c8c5f8b81b329079a1cea55a9454
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-06-17 14:05:31 +0100

    .

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ window.addEventListener('deviceorientation', function(event) {
     // $('images').textContent =  event.gamma
     if (event.gamma >= 0){
         $('images').textContent =  "HELLLO"
-        $('images').style.backgroundImage =  "url('./careworker.png')"
+        $('images').style.backgroundImage =  "url('./careworker.png') center center no-repeat;"
     }
     else{
         $('images').textContent =  "Goodbye"
-        $('images').style.backgroundImage =  "url('confidant.png')"
+        $('images').style.backgroundImage =  "url('confidant.png') center center no-repeat;"
     }
   });
 

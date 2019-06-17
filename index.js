f068911a6b17a1fac6be45492fdb3f5856fad6a0
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-06-17 12:56:24 +0100

    .

--- index.js
+++ index.js
@@ -15,6 +15,19 @@ accelerometer.start();
 // $('images').textContent = accelerometer.x
 
 
-window.addEventListener('devicemotion', function(event) {
-    $('images').innerHTML = event.acceleration.x ;
-  });
+// window.addEventListener('devicemotion', function(event) {
+//     $('images').innerHTML = event.acceleration.x ;
+//   });
+
+
+
+window.addEventListener('deviceorientation', function(eventData) {
+    // gamma is the left-to-right tilt in degrees
+    console.log(eventData.gamma);
+
+    // beta is the front-to-back tilt in degrees
+    $('images1').innerHTML = eventData.beta
+
+    // alpha is the compass direction the device is facing in degrees
+    $('images').innerHTML = eventData.alpha
+}, false);
